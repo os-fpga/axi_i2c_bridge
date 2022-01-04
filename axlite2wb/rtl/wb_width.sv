867b5ce5c351720936832671b5d9713322c527ca
Arshad Aleem <arshad.aleem@rapidsilicon.com>
2022-01-04 17:11:23 +0500

    separate out the ones counter module
    Signed-off-by: Arshad Aleem <arshad.aleem@rapidsilicon.com>

--- axlite2wb/rtl/wb_width.sv
+++ axlite2wb/rtl/wb_width.sv
@@ -437,23 +437,3 @@ module wb_width #(
     endgenerate
     
 endmodule
-
-module num_ones_for 
-#(  parameter WIDTH=4,
-    localparam COUNTER_SIZE = $clog2(WIDTH)
-    )(
-    input wire [WIDTH-1:0] A,
-    output reg [COUNTER_SIZE:0] ones
-    );
-
-integer i;
-
-    always@(A)
-    begin
-        ones = 0;  //initialize count variable.
-        for(i=0;i<WIDTH;i=i+1)   //check for all the bits.
-            if(A[i] == 1'b1)    //check if the bit is '1'
-                ones = ones + 1;    //if its one, increment the count.
-    end
-
-endmodule
