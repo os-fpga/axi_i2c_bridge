a6d841375b2f513112885c563eb840bedb42ea9f
Arshad Aleem <arshad.aleem@rapidsilicon.com>
2022-01-11 16:33:00 +0500

    remove macro redefined warnings
    Signed-off-by: Arshad Aleem <arshad.aleem@rapidsilicon.com>

--- i2c_core/rtl/i2c_master_defines.v
+++ i2c_core/rtl/i2c_master_defines.v
@@ -52,8 +52,12 @@
 // I2C registers wishbone addresses
 
 // bitcontroller states
-`define I2C_CMD_NOP   4'b0000
-`define I2C_CMD_START 4'b0001
-`define I2C_CMD_STOP  4'b0010
-`define I2C_CMD_WRITE 4'b0100
-`define I2C_CMD_READ  4'b1000
+`ifdef I2C_DEFINES
+`else
+    `define I2C_DEFINES
+    `define I2C_CMD_NOP   4'b0000
+    `define I2C_CMD_START 4'b0001
+    `define I2C_CMD_STOP  4'b0010
+    `define I2C_CMD_WRITE 4'b0100
+    `define I2C_CMD_READ  4'b1000
+`endif
